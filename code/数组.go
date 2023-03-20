6dbad9e2617048131feeb80050b109a6d8ec8c82
zijian <>
2023-03-20 09:43:20 +0800

    update

--- code/数组.go
+++ code/数组.go
@@ -83,3 +83,34 @@ func RemoveElement(nums []int, val int) int {
 }
 
 /******************************27. 移除元素******************************/
+
+// SearchInsert /******************************35. 搜索插入位置******************************/
+/*
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+请必须使用时间复杂度为 O(log n) 的算法。
+
+输入: nums = [1,3,5,6], target = 5
+输出: 2
+
+输入: nums = [1,3,5,6], target = 2
+输出: 1
+
+输入: nums = [1,3,5,6], target = 7
+输出: 4
+*/
+func SearchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
+}
+
+/******************************35. 搜索插入位置******************************/
