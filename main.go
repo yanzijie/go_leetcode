f9edcac223b6696513ed1ce3bedad24ae2d668f6
yh <981976482@qq.com>
2023-10-26 17:36:57 +0800

    update

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"github.com/yanzijie/go_leetcode/code"
-)
+import "fmt"
 
 func main() {
 	////7.翻转整数
@@ -48,13 +45,68 @@ func main() {
 	//list.Traverse()
 
 	//26.删除有序数组中的重复项
-	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	//nums := []int{10, 3, 2, 1, 1, 1, 6, 2, 3, 3, 4, 4, 6}
-	res := code.RemoveDuplicatesSlice(nums)
-	fmt.Println("res:", res)
+	//nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	////nums := []int{10, 3, 2, 1, 1, 1, 6, 2, 3, 3, 4, 4, 6}
+	//res := code.RemoveDuplicatesSlice(nums)
+	//fmt.Println("res:", res)
 
 	//64.二进制求和
 	//str := code.AddBinary("010","11")
 	//fmt.Println("AddBinary: ",str)
 
+	//ff(20)
+	factorial(100)
 }
+
+func ff(num int64) {
+	var i, n int64
+	n = 1
+	for i = 1; i <= num; i++ {
+		n = n * i
+		fmt.Println(i, n)
+	}
+}
+
+const MaxNum = 100000
+
+// 100的阶乘
+func factorial(num int) {
+	x := make([]int, MaxNum)
+	x[0] = 1
+	size := 1
+
+	for i := 2; i <= num; i++ {
+		size = multiply(x, size, i)
+
+	}
+	fmt.Println("---Factorial of", num, "is:")
+	for i := size - 1; i >= 0; i-- {
+		fmt.Print(x[i])
+	}
+	fmt.Println()
+}
+
+func multiply(x []int, size int, num int) int {
+	carry := 0
+	for i := 0; i < size; i++ {
+		prod := x[i]*num + carry
+		x[i] = prod % 10
+		carry = prod / 10
+	}
+
+	for carry != 0 {
+		x[size] = carry % 10
+		carry /= 10
+		size++
+	}
+
+	return size
+}
+
+//func f(size, num, l int, x []int) {
+//	fmt.Println(l, "---Factorial of", num, "is:")
+//	for i := size - 1; i >= 0; i-- {
+//		fmt.Print(x[i])
+//	}
+//	fmt.Println()
+//}
